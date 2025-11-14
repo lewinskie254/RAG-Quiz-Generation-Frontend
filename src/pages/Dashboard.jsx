ae9d505cdc2b1ad571f31232345c9469c637d996
lewinskie254 <machiulewis@gmail.com>
2025-11-14 21:29:11 +0300

    almost final commit of the day

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -17,6 +17,31 @@ const Dashboard = () => {
                         <SidePanelBtn title="Profile"/>
                     </ul>
                 </aside>
+                <div className="dashboard-content">
+                  <div className="card">
+                    <div className="card-content">
+                        <div className="score-tab">
+                            <h1 className='score-heading'>20</h1>
+                            <p className='score-paragraph'>Over <span className='out-of-tag'> 50</span> </p>
+                        </div>
+                        <div className="score-description">
+                            <h2 className='score-description-title'>Units Completed</h2>
+                        </div>
+                    </div>
+                    <div className="progress-bar">
+
+                    </div>
+                  </div>
+                  <div className="card">
+                        <h1>card 2</h1>
+                  </div>
+                  <div className="card">
+                        <h1>card 3</h1>
+                  </div>
+                  <div className="card">
+                        <h1>card 4</h1>
+                  </div>
+                </div>
             </div>
         </div>
     )
