c7a7fb04e0f0dc113b857b139bd6448a32460e68
lewinskie254 <machiulewis@gmail.com>
2025-11-14 15:31:30 +0300

    done with registration page, next stop student dashboard

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,9 +9,6 @@ const Register = () => {
     return (
     <div className="container">
       <div className="register">
-        <div className="logo-div">
-          <img src="src/assets/logo.jpg" alt="Logo" />
-        </div>
         <div className="input-div">
             <Title title="Registration Page" />
             <div className="input-section">
@@ -19,9 +16,18 @@ const Register = () => {
                 <InputField placeholder="Username" />
                 <InputField placeholder="Password"  type = "password"/>
                 <InputField placeholder="Confirm password"  type = "password"/>
+                <select className= "select-class" name="school" id="school"> 
+                    <option value="">Choose Your School</option> 
+                    <option value="aei">Ashley's Events Institute</option> 
+                    <option value="musk">Musk Global</option> 
+                    <option value="other">Other</option> 
+                </select>
                 <Button name="Register"/> 
                 <LoginOrRegister mainText= "Already a Member? " anchorText="Login Here" href="https://www.google.com" />
             </div>
+        </div>
+        <div className="logo-div">
+          <img src="src/assets/logo.jpg" alt="Logo" />
         </div>
       </div>
     </div>
