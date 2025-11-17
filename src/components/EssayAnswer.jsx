e056e7e2435d616c5767cb91b292452c3b119776
lewinskie254 <machiulewis@gmail.com>
2025-11-17 18:43:10 +0300

    done with the question view as well, now, i think we are done with the skeleton, other screens can be added later

---
+++ src/components/EssayAnswer.jsx
@@ -0,0 +1,9 @@
+const EssayAnswer = (props) => {
+    return (
+        <textarea name="" id={props.id} className="essay-answer" placeholder="Answer">
+
+        </textarea>
+    )
+}
+
+export default EssayAnswer; 
--- src/components/QuestionAndAnswer.jsx
+++ src/components/QuestionAndAnswer.jsx
@@ -4,18 +4,19 @@ import backUrl from "../assets/back.svg";
 import nextUrl from "../assets/next.svg"
 import ProgressBar from "../components/ProgressBar";
 import ImageBtn from "../components/ImageBtn";
+import EssayAnswer from "./EssayAnswer";
 
 
-const QuestionAndAnswer = () => {
+const QuestionAndAnswer = (props) => {
     return (
         <div className="question-and-answer">
             <Question question="This is the first question now and the provided timelines. "/> 
             <div className="answer-card">
                 <h2 className="answer-title">Answers</h2>
                 <div className="multiple-choice-answers">
-                    <MultipleChoiceAnswer answer="The first answer"/>
-                    <MultipleChoiceAnswer answer="The second answer"/>
-                    <MultipleChoiceAnswer answer="The third answer"/>
+                    <MultipleChoiceAnswer answer="The first answer"/> 
+                    <MultipleChoiceAnswer answer="The second answer"/> 
+                    <MultipleChoiceAnswer answer="The third answer"/> 
                     <MultipleChoiceAnswer answer="The fourth answer"/>
                 </div>
             </div>
