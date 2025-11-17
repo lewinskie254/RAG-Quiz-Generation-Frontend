1d8f4c18a8f9771fb39badba1f5a81cef722b223
lewinskie254 <machiulewis@gmail.com>
2025-11-17 17:37:03 +0300

    almost done with this thing

--- src/pages/Quizzes.jsx
+++ src/pages/Quizzes.jsx
@@ -23,7 +23,23 @@ const Quizzes = () => {
                     </ul>
                 </aside>
                 <div className="quizzes-content">
-
+                    <div className="question-and-answer">
+                        <div className="question-card">
+                            <h2 className="question-title">Question</h2>
+                            This is the sample of how the question will look like. 
+                            let's see how the question looks like when there isn an overflow of the content being asked. 
+                        </div>
+                        <div className="answer-card">
+                            <h2 className="answer-title">Answers</h2>
+                            <div className="multiple-choice-answers">
+                                <label class="check-container">
+                                    <input type="checkbox" />
+                                    <span class="checkmark"></span>
+                                    Accept terms
+                                </label>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
