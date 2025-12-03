da027846d8e50528cf0073c371f6a54e87de1633
lewinskie254 <machiulewis@gmail.com>
2025-12-03 18:17:13 +0300

    fixed the registration page's css

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -9,14 +9,14 @@ export default function Registration() {
 
     return (
     <div className="container">
-      <div className="register">
-        <div className="input-div">
+      <div className="register-page">
+        <div className="teacher-div">
             <div className="register-as-teacher">
                 <img src={Teacher}/>
                 <Button name="Register as teacher" inverse={true} onClick = {() =>  navigate('/register-teacher')}/>
             </div>
         </div>
-        <div className="logo-div">
+        <div className="student-div">
            <div className="register-as-student">
                 <img src={Student}/>
                 <Button name="Register as Student" onClick ={() =>  navigate('/register-student')}/>
